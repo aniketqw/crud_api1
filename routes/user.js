7c7df3e5deb4446c63d44a074da996b60b9e724c
aniketqw <aniket21stcentury@gmail.com>
2024-08-29 01:41:04 +0530

    CREATE delete /user/:id

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();//fresh router instance
 
 // mock database
 
-const users = [
+let users = [
     {
         first_name:'John',
         last_name:'Doe',
@@ -31,8 +31,73 @@ router.get('/',(req,res) => {
 
 router.get('/:id',(req,res) =>{
     const {id} =req.params;
-    const foundUser= user.find((user)=>user.id=== id)
+    const foundUser= users.find((user)=>user.id === id)
     res.send(foundUser)
-})
+});
+
+//CREATE delete /user/:id
+router.delete('/:id',(req,res) =>{
+    const {id}=req.params;
+    const {first_name}=req.params;
+    users =users.filter((user) => user.id!== id)
+    res.send(`${id} deleted successfuly with ${first_name} from database`);
+});
 
 export default router
+// import express from 'express';
+// import { v4 as uuidv4 } from 'uuid';
+
+// const router = express.Router();
+
+// // Mock database
+// let users = [
+//     {
+//         first_name: 'John',
+//         last_name: 'Doe',
+//     },
+//     {
+//         first_name: 'Alice',
+//         last_name: 'Smith',
+//     },
+// ];
+
+// // Adding users to our mock database
+// router.post('/', (req, res) => {
+//     const user = req.body;
+
+//     users.push({ ...user, id: uuidv4() });
+
+//     res.send(`${user.first_name} has been added to the Database`);
+// });
+
+// // Get all users
+// router.get('/', (req, res) => {
+//     res.send(users);
+// });
+
+// // Get a user by ID
+// router.get('/:id', (req, res) => {
+//     const { id } = req.params;
+//     const foundUser = users.find((user) => user.id === id);
+
+//     if (foundUser) {
+//         res.send(foundUser);
+//     } else {
+//         res.status(404).send('User not found');
+//     }
+// });
+
+// // Delete a user by ID
+// router.delete('/:id', (req, res) => {
+//     const { id } = req.params;
+//     const foundUser = users.find((user) => user.id === id);
+
+//     if (foundUser) {
+//         users = users.filter((user) => user.id !== id);
+//         res.send(`${foundUser.first_name} has been successfully deleted from the database`);
+//     } else {
+//         res.status(404).send('User not found');
+//     }
+// });
+
+// export default router;
